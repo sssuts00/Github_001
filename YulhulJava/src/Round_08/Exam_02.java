45c2c5878f66e9dce16c47aed55cd5b3e131a1c0
Administrator <Administrator@MSDN-SPECIAL>
2017-06-20 21:01:20 +0900

    ..

--- YulhulJava/src/Round_08/Exam_02.java
+++ YulhulJava/src/Round_08/Exam_02.java
@@ -3,8 +3,12 @@ package Round_08;
 public class Exam_02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int kor = 0, eng = 0, math = 0;
+		int [] sub = new int[3]; // default 자동 초기화
+				
+		int ko1 = 100, eng1 = 50, math1 = 70;
+		int [] sub1 = new int [] {100, 50, 70};
+		//                        [0]  [1] [2] => index(주소)자리
 	}
 
 }
