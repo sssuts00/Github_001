9c8bee5a749a20301d198de7db005c00b24ccb62
pd092 <pd092@DESKTOP-UH6AC6H>
2017-06-21 17:12:08 +0900

    17/06/21

---
+++ YulhulJava/src/Round_08/Exam_14.java
@@ -0,0 +1,18 @@
+package Round_08;
+
+public class Exam_14 {
+
+	public static void main(String[] args) {
+		int[][] a = new int [3][2];
+		
+		for(int i=0; i<a.length; ++i){
+			for(int j=0; j<2; ++j){
+				System.out.println("a["+ i +"]["+ j +"]= " + a[i][0]);
+			}
+			
+		}
+		
+
+	}
+
+}
